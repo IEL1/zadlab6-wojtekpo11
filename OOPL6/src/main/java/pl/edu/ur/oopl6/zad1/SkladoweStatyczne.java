34442754dd61f9007eabf76e5215ab105fbb4986
student <student@pc-108-21>
2018-04-09 17:41:28 +0200

    lab6

--- OOPL6/src/main/java/pl/edu/ur/oopl6/zad1/SkladoweStatyczne.java
+++ OOPL6/src/main/java/pl/edu/ur/oopl6/zad1/SkladoweStatyczne.java
@@ -4,9 +4,17 @@ public class SkladoweStatyczne {
 
     public static void main(String[] args) {
         // TODO zad 3
-        
+
         System.out.println(Figury.PoleKola(0.5));
         System.out.println(Figury.ObwodKola(0.5));
+        System.out.println(Figury.PoleKwadratu(2));
+        System.out.println(Figury.ObwodKwadratu(4));
+        System.out.println(Figury.PoleProstokata(4, 3));
+        System.out.println(Figury.PoleStozka(4, 3));
+        System.out.println(Figury.PoleWalca(2, 5));
+        System.out.println(Figury.ObjetoscWalca(2, 10));
+        System.out.println(Figury.ObjetoscStozka(4, 6));
+        System.out.println(Figury.ObwodProstokata(2, 20));
     }
-    
+
 }
